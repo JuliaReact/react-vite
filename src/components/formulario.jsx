3b58c78da5267a6fc689f2cf1453845e9a6a6040
JuliaReact <luniclara@gmail.com>
2024-03-15 20:16:37 +0100

    cambios formulario

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -3,13 +3,15 @@ import { useState } from "react";
 const formulario = () => {
     // eslint-disable-next-line no-unused-vars
     const [nombre, setNombre] = useState
+    const [apellido, setApellido] = useState
     const [contraseña, setcontraseña] = useState
     return ( 
         <div>
             <h1>FORMULARIO DE DATOS</h1>
             <form >
-                <input type='text' id='nombre' value={nombre} >NOMBRE</input>
-                <input type="password" id="password" value={contraseña}>USUARIO</input>
+                <input type='text' id='nombre' value={nombre} onChange={ e => setNombre(e.target.value)} >NOMBRE</input>
+                <input type='text' id='apellido' value={apellido} onChange={ e => setApellido(e.target.value)}>APELLIDO</input>
+                <input type="password" id="password" value={contraseña} onChange={ e => setcontraseña(e.target.value)} >USUARIO</input>
                 <button type="submit">+</button>
             </form>
             
